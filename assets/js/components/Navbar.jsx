021044bb22c08ee2e817541c9f35160a26890604
CSRGH27 <universaltaskmail@gmail.com>
2020-05-22 15:30:22 +0200

    Add Switch Router

---
+++ assets/js/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const Navbar = (props) => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">
+        SymReact
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarColor02"
+        aria-controls="navbarColor02"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarColor02">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Customers
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Invoices
+            </a>
+          </li>
+        </ul>
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              Registration
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="btn btn-primary">
+              Login
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="btn btn-danger">
+              Logout
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ assets/js/pages/CustomersPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomersPage = (props) => {
+  return <h1>List of clients</h1>;
+};
+
+export default CustomersPage;
